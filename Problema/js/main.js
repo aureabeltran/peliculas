d3daf1e591c2812bb1c5da544b5225423bcc4a10
bittersweet <aureabeltranmanzo@gmail.com>
2016-07-16 19:58:52 -0400

    first commit

---
+++ Problema/js/main.js
@@ -0,0 +1,58 @@
+var ciencia = $(".ciencia");
+var romantica = $(".romantica");
+var infantil = $(".infantil");
+var terror = $(".terror");
+var accion = $(".accion");
+var row = $(".row");
+var colocar =  $(".colocar");
+
+function all (element){
+	infantil.show();
+	ciencia.show();
+	romantica.show();
+	terror.show();
+	accion.show();
+	colocar.addClass("row");
+}
+function kids (element) {
+	infantil.show();
+	ciencia.hide();
+	romantica.hide();
+	terror.hide();
+	accion.hide();
+	row.removeClass();
+}
+function scifi (element) {
+	ciencia.show();
+	infantil.hide();
+	romantica.hide();
+	terror.hide();
+	accion.hide();
+	row.removeClass();
+}
+function love (element){
+	romantica.show();
+	infantil.hide();
+	terror.hide();
+	accion.hide();
+	ciencia.hide();
+	row.removeClass();
+}
+function action (element) {
+	romantica.hide();
+	infantil.hide();
+	romantica.hide();
+	terror.hide();
+	accion.show();
+	ciencia.hide();
+	row.removeClass();
+}
+function scary (element){
+	romantica.hide();
+	infantil.hide();
+	romantica.hide();
+	terror.show();
+	accion.hide();
+	ciencia.hide();
+	row.removeClass();
+}
